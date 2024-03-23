3abe938866ffbfe408c95592be3733dc4211b732
toche <t.samanchuen.gmail.com>
2024-03-23 13:47:28 +0700

    Add game over functionality and restart option

--- main.py
+++ main.py
@@ -5,10 +5,59 @@ pygame.init()
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
+oldkey = 0
+font = pygame.font.Font(None, 36)
 
 def gen_start_pos():
     return (random.randint(0, 1280), random.randint(0, 720))
 
+# function print you lose when the snake hit the wall or itself on the game screen
+def you_lose():
+    #show you lose on the screen
+    text = font.render("You Lose", True, "red")
+    screen.blit(text, (600, 360))
+    pygame.display.flip()
+    pygame.time.delay(2000)
+
+
+#ask for restart
+def ask_for_restart():
+    text = font.render("Press R to restar or Esc for Quit", True, "red")
+    screen.blit(text, (470, 260))
+    pygame.display.flip()
+    # wait for user to press R to restart
+    while True:
+        event = pygame.event.wait()
+        global running
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_r:
+                restart()
+                break
+            elif event.key == pygame.K_ESCAPE:
+                running = False
+                break
+        elif event.type == pygame.QUIT:
+            running = False
+            break
+
+# restart the game
+def restart():
+    global running
+    running = True
+    global player_pos
+    player_pos = pygame.Vector2(gen_start_pos())
+    global snake_pixel
+    snake_pixel.center = player_pos
+    global old_pos
+    old_pos = player_pos.copy()
+    global snake_length
+    snake_length = 1
+    global snake
+    snake = [snake_pixel.copy()]
+    global food
+    food.center = pygame.Vector2(gen_start_pos())
+    global oldkey
+    oldkey = 0
 
 #playground
 pixel_width = 50
@@ -22,13 +71,6 @@ snake = [snake_pixel.copy()]
 food = pygame.Rect(0, 0, pixel_width, pixel_width)
 food.center = pygame.Vector2(gen_start_pos())
 
-
-
-
-# print(snake_pixel)
-# print(snake_pixel.center)
-# input("Press enter to continue")
-
 while running:
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
@@ -39,8 +81,8 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("black")
 #    show score on screen
-    font = pygame.font.Font(None, 36)
-    score_text = font.render(f"Score: {snake_length-1}", True, "white")
+
+    score_text = font.render(f"Score: {snake_length-1}", True, "blue")
     screen.blit(score_text, (0, 0))
     
     # RENDER YOUR GAME HERE
@@ -50,6 +92,17 @@ while running:
             snake = snake[:snake_length-1]
             snake.insert(0, snake[0].copy())
         snake[0].center = player_pos
+
+    # snake collision itself
+    for segment in snake[1:]:
+        if snake[0].colliderect(segment): 
+             you_lose()
+             ask_for_restart()
+
+    # snake collision the wall
+    if player_pos.x < 0 or player_pos.x > 1280 or player_pos.y < 0 or player_pos.y > 720:
+         you_lose()
+         ask_for_restart()
 
     for segment in snake:
         pygame.draw.rect(screen, "green", segment)
@@ -62,21 +115,21 @@ while running:
         snake.append(snake[-1].copy())
         print(snake_length)
     
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= pixel_width
-    if keys[pygame.K_s]:
-        player_pos.y += pixel_width
-    if keys[pygame.K_a]:
-        player_pos.x -= pixel_width
-    if keys[pygame.K_d]:
-        player_pos.x += pixel_width
-    
-
+    if event.type == pygame.KEYDOWN:
+        oldkey = event.key  
+    if oldkey == pygame.K_UP:
+            player_pos.y -= pixel_width
+    elif oldkey == pygame.K_DOWN:
+            player_pos.y += pixel_width
+    elif oldkey == pygame.K_LEFT:
+            player_pos.x -= pixel_width
+    elif oldkey == pygame.K_RIGHT:
+            player_pos.x += pixel_width
+        
+        
     # flip() the display to put your work on screen
     pygame.display.flip()
 
-    clock.tick(15)  # limits FPS to 60
+    clock.tick(8)  # limits FPS to 60
 
 pygame.quit()
